025d1ce8751169bf6f80289f15eed0fee894841e
Rhys Takahashi <19mt01@gmail.com>
2026-05-18 16:26:02 -0400

    cleaned up, added shared state + IOC

--- local_dispatch.py
+++ local_dispatch.py
@@ -15,14 +15,20 @@ import requests
 if __name__ == "__main__":
     trigger, out_q = tpi.test_boot()
     if sys.argv[1] is not None:
-        requests.get(str(sys)+"/measurement/start")
+        requests.get('https://'+str(sys.argv[1])+"/measurement/start")
     else:
         requests.get('https://'+tpi.HOST +":"  + tpi.SERVAL+"/measurement/start")
 
     trigger.set()
+    cycler = 0
     while trigger.is_set():
-        print("[monitor]\t stream is still set (yay) waiting")
-        time.sleep(5)
+        if cycler == 0:
+            print("[monitor]\t stream is still set (yay) waiting")
+        cycler += 1
+        cycler %= 30
+        time.sleep(1)
+    
+    print("[monitor]\t stream has ended")
 
 
 
